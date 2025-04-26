b79b14ecd2ab5a4f0b3c89a143094cb6662e2415
inbarajan <inbarajan.wizinoa@gmail.com>
2025-04-26 12:33:33 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black font-poppins flex justify-center items-center min-h-dvh">
+      <p className="font-medium text-white leading-6 tracking-wide text-xl">
+        Welcome to our booking application!
+      </p>
+    </div>
+  );
+}
+
+export default App;
